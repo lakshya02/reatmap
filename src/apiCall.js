8fa40a7ac727edeb3f370a5b7d93be57e64daa3e
mustafa kahraman <kahramanmm@gmail.com>
2018-05-30 23:12:20 -0400

    buffer conversion fixed

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,31 +1,24 @@
 import axios from 'axios';
 import protobuf from 'protobufjs';
 
-let getUpdate = ()=>{
-    return axios({
-        method:'get',
-        url:'http://localhost:3000/fetchupdate',
-        headers: {'Content-Type': 'application/octet-stream'}
-    }).then(payload=>{
-      protobuf.load("./test.proto", function(err, root) {
-          payload = atob(payload.data);
-          var FeedMessage = root.lookupType("test_realtime.FeedMessage");
-          // var errMsg = FeedMessage.verify(payload);
-          // if (errMsg)
-          //   throw Error(errMsg);
-          console.log(FeedMessage.decode(payload).toJSON())
-      })
+let getUpdate = () => {
+  return axios({
+    method: 'get',
+    url: 'http://localhost:3000/fetchupdate',
+    headers: { 'Content-Type': 'application/octet-stream' }
+  }).then(payload => {
 
-        // protobuf.load("./test.proto", function(err, root) {
-        //     if (err)
-        //       throw err;
-        //     //   var FeedMessage = root.lookupType("test_realtime.FeedMessage");
-        //     //   var errMsg = FeedMessage.verify(payload);
-        //     //   if (errMsg)
-        //     //     throw Error(errMsg);
-        //     //   console.log(FeedMessage.decode(payload).toJSON())
-        //     })  
-        })
+    var root = new protobuf.Root();
 
-      }
+    root.load("./test.proto", { keepCase: true }, function (err, root) {
+      var FeedMessage = root.lookupType("test_realtime.FeedMessage");
+      let buffer = payload.data.body.data
+      var demessage = FeedMessage.decode(buffer);
+      var json = demessage.toJSON();
+      console.log(JSON.stringify(json));
+
+    })
+  })
+
+}
 export default getUpdate;
